24b7bd323980a7dabd9cd67240f8a14d719dee6b
KALIPEM <kalidaspem@gmail.com>
2024-08-15 23:59:04 +0530

    dragon repeller steps 12 - 20 complete

--- day-4/script.js
+++ day-4/script.js
@@ -1,7 +1,12 @@
-let xp = 0;
-let health = 100;
-let gold = 50;
-let currentWeaponIndex = 0;
-let fighting;
-let monsterHealth;
-let inventory = ["stick"];
+let xp = 0; // experience points
+let health = 100; // health points
+let gold = 50; // amount of gold
+let currentWeaponIndex = 0; // index of the current weapon in the weapons array
+let fighting; 
+let monsterHealth; // health of the monster
+let inventory = ["stick"]; // an array of weapons
+
+// DOM elements
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
